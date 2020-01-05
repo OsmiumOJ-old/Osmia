5489207b9f5d2809125ce50bdfe65ecd5f8177a1
OsmiumOJ <OsmiumOJ@qq.com>
2020-01-05 22:10:05 +0800

    1

---
+++ osmia.bat
@@ -0,0 +1,28 @@
+@echo off
+set progp=C:\ProgramData\Microsoft\Windows\Start Menu\Programs
+if "%1" == "" (
+    type pfi\noargs.pfi
+    goto end
+) else if "%1" == "--help" ( goto hlp
+) else if "%1" == "-h" ( goto hlp
+) else if "%1" == "/h" ( goto hlp
+) else if "%1" == "-?" ( goto hlp
+) else if "%1" == "/?" ( goto hlp
+) else if "%1" == "--version" ( goto vers
+) else if "%1" == "-v" ( goto vers
+) else if "%1" == "/v" ( goto vers
+) else ( goto err )
+:hlp
+if "%2" == "" (
+    type pfi\help.pfi
+    goto end
+) else ( goto err )
+:vers
+if "%2" == "" (
+    type pfi\version.pfi
+    goto end
+) else ( goto err )
+:err
+echo Error.
+:end
+pause
